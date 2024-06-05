0bc29571df005a3714403e65f92a6c6d0b1d9c37
Cecilia Heinefors <katttiger@gmail.com>
2024-06-05 13:27:38 +0200

    Fixed naming and added methods.

--- Moo/Program.cs
+++ Moo/Program.cs
@@ -11,36 +11,36 @@ namespace MooGame
             bool playOn = true;
             Console.WriteLine("Enter your user name:\n");
             string name = Console.ReadLine();
+            //GameEngine engine = new GameEngine();
 
             while (playOn)
             {
-                string goal = CreateGoal();
 
+                string goal = GameEngine.CreateGoal();
                 Console.WriteLine("New game:\n");
 
                 //comment out or remove next line to play real games!
                 Console.WriteLine("For practice, number is: " + goal + "\n");
 
-                string guess = Console.ReadLine();
-                int numberOfGuess = 1;
-                string bullsAndCows = CheckBullsAndCows(goal, guess);
-                Console.WriteLine(bullsAndCows + "\n");
-
-                while (bullsAndCows != "BBBB,")
+                int numberOfGuesses = 1;
+                string bullsAndCows = string.Empty;
+                while (!bullsAndCows.Contains("BBBB,"))
                 {
-                    numberOfGuess++;
-                    guess = Console.ReadLine();
-                    //Console.WriteLine(guess + "\n");
-                    bullsAndCows = CheckBullsAndCows(goal, guess);
-                    Console.WriteLine(bullsAndCows + "\n");
+                    bullsAndCows = GameEngine.GuessTheNumber(goal);
+                    numberOfGuesses++;
                 }
 
+                //Scoreboard => seperate part of the code?
+                //Fetch the result-file
                 StreamWriter output = new StreamWriter("result.txt", append: true);
-                output.WriteLine(name + "#&#" + numberOfGuess);
+                output.WriteLine(name + "#&#" + numberOfGuesses);
                 output.Close();
+
                 ShowTopList();
+
+
                 //Clearer instructions (y/n and catch)
-                Console.WriteLine("Correct, it took " + numberOfGuess + " guesses. \nContinue?");
+                Console.WriteLine("Correct, it took " + numberOfGuesses + " guesses. \nContinue?");
                 string answer = Console.ReadLine();
                 if (answer != null && answer != "" && answer.Substring(0, 1) == "n")
                 {
@@ -49,51 +49,7 @@ namespace MooGame
             }
         }
 
-        static string CreateGoal()
-        {
-            Random NumberGenerator = new Random();
-            string goal = "";
-            for (int i = 0; i < 4; i++)
-            {
-                int random = NumberGenerator.Next(10);
-                string randomDigit = "" + random;
-                while (goal.Contains(randomDigit))
-                {
-                //append?
-                    random = NumberGenerator.Next(10);
-                    randomDigit = "" + random;
-                }
-                goal = goal + randomDigit;
-            }
-            return goal;
-        }
-
-        static string CheckBullsAndCows(string goal, string guess)
-        {
-            int cows = 0, bulls = 0;
-            //If player entered more than four characters?
-            //If player entered less than four characters?
-            guess += "    ";     // if player entered less than 4 chars => increased looptime.
-            for (int i = 0; i < 4; i++)
-            {
-                for (int j = 0; j < 4; j++)
-                {
-                    if (goal[i] == guess[j])
-                    {
-                        if (i == j)
-                        {
-                            bulls++;
-                        }
-                        else
-                        {
-                            cows++;
-                        }
-                    }
-                }
-            }
-            return "BBBB".Substring(0, bulls) + "," + "CCCC".Substring(0, cows);
-        }
-
+        //shoulb be small an do one thing
         static void ShowTopList()
         {
             StreamReader input = new StreamReader("result.txt");
@@ -125,19 +81,90 @@ namespace MooGame
         }
     }
 
+    //TODO: Consider whether the enigne should be static or not.
+    public static class GameEngine
+    {
+        public static string CreateGoal()
+        {
+            Random randomGenerator = new Random();
+            string goal = "";
+            for (int i = 0; i < 4; i++)
+            {
+                int random = randomGenerator.Next(10);
+                string randomDigit = "" + random;
+                while (goal.Contains(randomDigit))
+                {
+                    random = randomGenerator.Next(10);
+                    randomDigit = "" + random;
+                }
+                goal = goal + randomDigit;
+            }
+            return goal;
+        }
+        public static string GuessTheNumber(string goal)
+        {
+            string guess = Console.ReadLine();
+            string bullsAndCows = CheckIfGuessIsValid(goal, guess);
+            Console.WriteLine(bullsAndCows + "\n");
+            return bullsAndCows;
+        }
+        static string CheckIfGuessIsValid(string goal, string guess)
+        {
+            if (guess.Length > 4)
+            {
+                return "Your guess has more than 4 digits.";
+            }
+            else if (guess.Length < 4)
+            {
+                return "Your guess has less than 4 digits.";
+            }
+            else
+            {
+                return CheckBullsAndCows(goal, guess);
+            }
+        }
+        static string CheckBullsAndCows(string goal, string guess)
+        {
+            int bulls = 0;
+            int cows = 0;
+
+            //i=>length of guess (4)
+            //j=>count the number in the sequence
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    if (goal[i] == guess[j])
+                    {
+                        if (i == j)
+                        {
+                            bulls++;
+                        }
+                        else
+                        {
+                            cows++;
+                        }
+                    }
+                }
+            }
+            //I think this needs polishing:
+            //Goal: 0377
+            //Guess: 0070
+            //Result: BB,CCC => maybe ask user to insert unique numbers?
+            return "BBBB".Substring(0, bulls) + "," + "CCCC".Substring(0, cows);
+        }
+    }
+
     class PlayerData
     {
-        //Creates a new player even if a player has the same name.
         public string Name { get; private set; }
-
-        //Shouldn't it be updated when you have played a new round?
-        //NumberOfRoundsPlayed
         public int NumberOfGamesPlayed { get; private set; }
         int totalGuesses;
 
         public PlayerData(string name, int guesses)
         {
             this.Name = name;
+            //Number of rounds?
             NumberOfGamesPlayed = 1;
             totalGuesses = guesses;
         }
@@ -145,6 +172,7 @@ namespace MooGame
         public void UpdatePlayerScore(int guesses)
         {
             totalGuesses += guesses;
+            //Number of rounds?
             NumberOfGamesPlayed++;
         }
         public double CalculatePlayerAverageScore()
