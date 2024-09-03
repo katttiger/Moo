7065f24eace6f482452a840748c5755a55efe1cd
Cecilia Heinefors <katttiger@gmail.com>
2024-09-03 12:12:27 +0200

    Edited IGame.

--- Moo/Games/IGame.cs
+++ Moo/Games/IGame.cs
@@ -6,8 +6,5 @@
         string PathToScore { get; set; }
         public void Display();
         public int GameLogic();
-        public string CreateGoal();
-        void CreatePlayer();
-        void SavePlayerdata();
     }
 }
